8022615fd6d976d5d2f14ce9666458bafb7f0be9
jens harms <au1064@gmail.com>
2020-06-25 10:57:54 +0200

    working

--- json_reader/json_read.c
+++ json_reader/json_read.c
@@ -4,6 +4,7 @@
 */
 
 #include "mls.h"
+#include "json_read.h"
 #include "json_parse.tab.h"
 #include "json_lex.lex.h"
 #include <limits.h>
@@ -11,109 +12,16 @@
 
 int yyparse(void);
 
-/* _vname == OBJ */
-static int LIST = 0;
-static int STACK = 0;
-static int ARG = 0;
 static int MEM = 0;
-
-static int create_arg(char *first)
-{
-    char *null = NULL;
-    char * v = strdup(first);
-    int a = m_create(2,sizeof(char*));
-    m_put(a,&null); m_put(a,&v);
-    return a;
-}
-
-int json_init( FILE *fp )
-{
-    STACK = m_create(20,sizeof(int));
-    LIST = 0;
-    ARG = 0;
-    MEM = m_create(20,sizeof(char*));
-    yyin=fp;
-    yyparse();
-    m_free_strings( MEM, 0 );
-    m_free(STACK);
-    m_free_strings(ARG,0); /* root arg object */ 
-    return LIST;
-}
-
-
-/* push current LIST, create a new LIST */
-void json_object_push(void)
-{
-    TRACE(1,"OLD: %d", LIST);
-    m_put( STACK, &LIST );
-    LIST = v_init();
-    TRACE(1,"CUR: %d", LIST);
-}
-
-    
-/*  set ARG to current list-obj, pop LIST from stack */
-void json_object_pop(void)
-{
-    char num[20];
-    snprintf(num, sizeof num, "%c%d",0x01, LIST );
-    ARG = create_arg( num );
-
-    int * v = m_pop(STACK); 
-    if( v && *v ) LIST = *v; /* do not pop last ITEM */
-    TRACE(1,"CUR: %d", LIST);
-}
-
-
-/* create ARG if not exists, prepend ARG arguments with arg */
-void json_array( char *arg )
-{
-    TRACE(1,"%s", arg );
-    if( !ARG ) {
-	ARG = create_arg( arg );
-	return;
-    }
-    char * v = strdup(arg);
-    m_ins( ARG, 1, 1 );
-    m_write( ARG,1, &v, 1);
-}
-
-void json_pair( char *name, char *val )
-{
-    TRACE(1,"%s:%s", name,val );
-    if( !ARG ) {
-	ARG = create_arg( val );
-    }
-    char *v = strdup(name);
-    m_write( ARG,0, &v, 1);
-
-    TRACE(1,"%d,%d", LIST, ARG );
-    m_put( LIST, &ARG );
-    ARG = 0;
-}
 
 void json_stack( char *arg )
 {
     m_put(MEM,&arg);
 }
 
-
-
-enum json_typ {
-	       JSON_STRING,
-	       JSON_NUM,
-	       JSON_BOOL,
-	       JSON_NULL,
-	       JSON_ARR,
-	       JSON_OBJ
-};
-
 static int stack2 = 0;
-struct json_st {
-    int name;
-    int typ;
-    int d;
-};
 static struct json_st *current, *root;
+static struct json_st ROOT;
 
 static void d_elem( struct json_st *obj );
 static void d_obj(int l)
@@ -170,7 +78,7 @@ void json_new(char *name, int t)
     struct json_st *json;
     if( stack2 == 0 ) {
 	stack2=m_create(50,sizeof(struct json_st*));
-	root = malloc(sizeof(struct json_st));
+	root = &ROOT;
 	root->d = m_create(10,sizeof(struct json_st));
 	root->typ = t;
 	root->name = 0;
@@ -210,4 +118,14 @@ void json_name(char*name)
     struct json_st *json =
 	mls(current->d, m_len(current->d)-1);
     json->name = s_printf(0,0,"%s", name );
+}
+
+int json_init( FILE *fp )
+{
+    MEM = m_create(20,sizeof(char*));
+    yyin=fp;
+    yyparse();
+    m_free_strings( MEM, 0 );
+
+    return ROOT.d;
 }
